d40ce2b709a1f1ce1c2a5cd9dbbca26015941bf7
joao-parana <joao.parana@gmail.com>
2013-09-16 14:14:06 -0300

    alterações de modularização de código

--- src/persistence/Dao.java
+++ src/persistence/Dao.java
@@ -4,19 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Map;
 
 public class Dao {
-	
+	static boolean firstTime = true;
 	Connection con;
 	PreparedStatement stmt;
 	ResultSet rs;
-		
-	public void open() throws Exception{
+
+	public void open() throws Exception {
+		if (firstTime) {
+			Map<String, String> env = System.getenv();
+			for (String envName : env.keySet()) {
+				System.out.format("%s=%s%n", envName, env.get(envName));
+			}
+			firstTime = false;
+		}
+
 		Class.forName("com.mysql.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ajax", "root", "bonfim");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ajax",
+				"teste", "teste");
 	}
-	
-	public void close() throws Exception{
+
+	public void close() throws Exception {
 		con.close();
 	}
 
